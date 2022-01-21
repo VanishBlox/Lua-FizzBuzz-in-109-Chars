89152b731f02420b79b3091829787b449720d7cc
Vanishblox <45913821+VanishBlox@users.noreply.github.com>
2022-01-21 22:08:00 +0100

    106 chars actually

--- FizzBuzz.lua
+++ FizzBuzz.lua
@@ -1,1 +1,1 @@
-for i=1,100 do P="" if i%3==0 then P="Fizz"end if i%5==0 then P=P.."Buzz"end if P==""then P=i end print(P)end
+for i=1,100 do P=""if i%3<1 then P="Fizz"end if i%5<1 then P=P.."Buzz"end if P==""then P=i end print(P)end
